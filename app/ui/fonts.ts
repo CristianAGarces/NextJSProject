cb8c6b574a3ee74869be4ccaedc9540473e4c485
CristianAGarces <cristyan@users.noreply.github.com>
2024-12-06 12:31:12 -0500

    correct error

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Coming_Soon, Lusitana, Montserrat } from "next/font/google";
+
+export const montserrat = Montserrat({ subsets: ["latin"] });
+export const comit = Coming_Soon({ subsets: ["latin"],weight:["400"] });
+export const lusitana = Lusitana({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+
